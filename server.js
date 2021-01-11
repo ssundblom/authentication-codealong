7e84cd7904587c522a83987be6520b3638f0af2b
Sara Sundblom <sundblomsara@gmail.com>
2021-01-11 13:26:37 +0100

    authentication code along very good

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(123).toString('hex')
+  }
+})
+
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({ accessToken: req.header('Authorization') })
+  if (user) {
+    req.user = user
+    next()
+  } else {
+    res.status(401).json({ loggedOut: true })
+  }
+}
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -15,7 +46,35 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello schemllo')
+})
+
+app.post('/users', async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+    const SALT = bcrypt.genSaltSync(10);
+    const user = new User({ name, email, password: bcrypt.hashSync(password, SALT) })
+    user.save()
+    res.status(201).json({ id: user._id, accessToken: user.accessToken })
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({ message: 'Could not create juser', errors: err.errors })
+  }
+})
+
+app.get('/secrets', authenticateUser)
+app.get('/secrets', (req, res) => {
+  res.json({ secret: 'This is super secret schhhhh' })
+})
+
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({email: req.body.email})
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({userId: user._id, accessToken: user.accessToken})
+  } else {
+    res.json({notFound: true})
+  }
+
 })
 
 // Start the server
